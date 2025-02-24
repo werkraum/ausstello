a1f64c40a4f1ab26b4ba19204b02f40e3b2c07ce
Lukas Niestroj <lukas.niestroj@werkraum.net>
2025-02-24 10:27:34 +0100

    add highlight events if marked as highlight

--- Resources/Public/JavaScript/ausstello.js
+++ Resources/Public/JavaScript/ausstello.js
@@ -1,3 +1,5 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = "http://localhost:35729/livereload.js?snipver=1"; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 /**
  * SSR Window 4.0.2
  * Better handling for window object in SSR environment
