d85e17abd3d74cdd7907a5d04c18ca34dbd6f19f
Lukas Niestroj <lukas.niestroj@werkraum.net>
2025-02-27 10:18:30 +0100

    align category icon update hover reorder detail button and tag update mobile image/slider/lightbox

--- Build/JavaScript/detail.js
+++ Build/JavaScript/detail.js
@@ -14,85 +14,65 @@ import 'photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css'
 import "../Scss/ausstello.scss";
 
 const setupSwiper = () => {
-
-  const swiper = new Swiper('.swiper', {
-    modules: [Pagination],
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    keyboard: {
-      enabled: true,
-    }
-  });
-
-  swiper.on('slideChange', () => {
-    const activeIndex = swiper.realIndex;
-    const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
-
-    paginationBullets.forEach((bullet, index) => {
-      if (index === activeIndex) {
-        bullet.classList.add('swiper-pagination-bullet-active');
-      } else {
-        bullet.classList.remove('swiper-pagination-bullet-active');
-      }
-    });
-  });
-
   const photo_swipe_options = {
     gallery: '#lightbox-gallery',
     pswpModule: () => import("photoswipe"),
-    // set background opacity
-    // bgOpacity: 1,
-    // showHideOpacity: true,
     children: 'a',
     initialZoomLevel: 'fit',
-    // loop: true,
-    // showHideAnimationType: 'zoom', /* options: fade, zoom, none */
-
-    /* Click on image moves to the next slide */
-    // imageClickAction: 'next',
-    // tapAction: 'next',
-
-    /* ## Hiding a specific UI element ## */
-    // zoom: false,
-    // close: true,
-    // counter: true,
-    // arrowKeys: true,
-    /* ## Options ## */
-    // wheelToZoom: true, /* deafult: undefined */
   };
-
   const lightbox = new PhotoSwipeLightbox(photo_swipe_options);
-
-  const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+  new PhotoSwipeDynamicCaption(lightbox, {
     // Plugins options, for example:
     type: 'below',
   });
-
   lightbox.init();
 
-  lightbox.on('change', () => {
-    const { pswp } = lightbox;
-    swiper.slideTo(pswp.currIndex, 0, false);
-  });
+  const swiperContainer = document.querySelector('.swiper');
+  if (swiperContainer) {
+    const swiper = new Swiper('.swiper', {
+      modules: [Pagination],
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination'
+      },
+      keyboard: {
+        enabled: true,
+      }
+    });
+    swiper.on('slideChange', () => {
+      const activeIndex = swiper.realIndex;
+      const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
 
-  /* ### PhotoSwipe events ### */
+      paginationBullets.forEach((bullet, index) => {
+        if (index === activeIndex) {
+          bullet.classList.add('swiper-pagination-bullet-active');
+        } else {
+          bullet.classList.remove('swiper-pagination-bullet-active');
+        }
+      });
+    });
 
-  lightbox.on('afterInit', () => {
-    if(swiper.params?.autoplay?.enabled){
-      swiper.autoplay.stop();
-    }
-  });
+    lightbox.on('change', () => {
+      const { pswp } = lightbox;
+      swiper.slideTo(pswp.currIndex, 0, false);
+    });
 
-  lightbox.on('closingAnimationStart', () => {
-    const { pswp } = lightbox;
-    swiper.slideTo(pswp.currIndex, 0, false);
-    /* if autoplay enabled == true -> autoplay.start() when close lightbox */
-    if(swiper.params?.autoplay?.enabled){
-      swiper.autoplay.start();
-    }
-  });
+    lightbox.on('afterInit', () => {
+      if(swiper.params?.autoplay?.enabled){
+        swiper.autoplay.stop();
+      }
+    });
+
+    lightbox.on('closingAnimationStart', () => {
+      const { pswp } = lightbox;
+      swiper.slideTo(pswp.currIndex, 0, false);
+      /* if autoplay enabled == true -> autoplay.start() when close lightbox */
+      if(swiper.params?.autoplay?.enabled){
+        swiper.autoplay.start();
+      }
+    });
+  }
+
 }
 
 (() => {
--- Resources/Public/JavaScript/detail.js
+++ Resources/Public/JavaScript/detail.js
@@ -6833,49 +6833,11 @@ class PhotoSwipeDynamicCaption {
     return captionHTML;
   }
 }const setupSwiper = () => {
-  const swiper = new Swiper('.swiper', {
-    modules: [Pagination],
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    keyboard: {
-      enabled: true
-    }
-  });
-  swiper.on('slideChange', () => {
-    const activeIndex = swiper.realIndex;
-    const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
-    paginationBullets.forEach((bullet, index) => {
-      if (index === activeIndex) {
-        bullet.classList.add('swiper-pagination-bullet-active');
-      } else {
-        bullet.classList.remove('swiper-pagination-bullet-active');
-      }
-    });
-  });
   const photo_swipe_options = {
     gallery: '#lightbox-gallery',
     pswpModule: () => import('./photoswipe.esm-DcNoHvvq.js'),
-    // set background opacity
-    // bgOpacity: 1,
-    // showHideOpacity: true,
     children: 'a',
     initialZoomLevel: 'fit'
-    // loop: true,
-    // showHideAnimationType: 'zoom', /* options: fade, zoom, none */
-
-    /* Click on image moves to the next slide */
-    // imageClickAction: 'next',
-    // tapAction: 'next',
-
-    /* ## Hiding a specific UI element ## */
-    // zoom: false,
-    // close: true,
-    // counter: true,
-    // arrowKeys: true,
-    /* ## Options ## */
-    // wheelToZoom: true, /* deafult: undefined */
   };
   const lightbox = new PhotoSwipeLightbox(photo_swipe_options);
   new PhotoSwipeDynamicCaption(lightbox, {
@@ -6883,30 +6845,51 @@ class PhotoSwipeDynamicCaption {
     type: 'below'
   });
   lightbox.init();
-  lightbox.on('change', () => {
-    const {
-      pswp
-    } = lightbox;
-    swiper.slideTo(pswp.currIndex, 0, false);
-  });
-
-  /* ### PhotoSwipe events ### */
-
-  lightbox.on('afterInit', () => {
-    if (swiper.params?.autoplay?.enabled) {
-      swiper.autoplay.stop();
-    }
-  });
-  lightbox.on('closingAnimationStart', () => {
-    const {
-      pswp
-    } = lightbox;
-    swiper.slideTo(pswp.currIndex, 0, false);
-    /* if autoplay enabled == true -> autoplay.start() when close lightbox */
-    if (swiper.params?.autoplay?.enabled) {
-      swiper.autoplay.start();
-    }
-  });
+  const swiperContainer = document.querySelector('.swiper');
+  if (swiperContainer) {
+    const swiper = new Swiper('.swiper', {
+      modules: [Pagination],
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination'
+      },
+      keyboard: {
+        enabled: true
+      }
+    });
+    swiper.on('slideChange', () => {
+      const activeIndex = swiper.realIndex;
+      const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
+      paginationBullets.forEach((bullet, index) => {
+        if (index === activeIndex) {
+          bullet.classList.add('swiper-pagination-bullet-active');
+        } else {
+          bullet.classList.remove('swiper-pagination-bullet-active');
+        }
+      });
+    });
+    lightbox.on('change', () => {
+      const {
+        pswp
+      } = lightbox;
+      swiper.slideTo(pswp.currIndex, 0, false);
+    });
+    lightbox.on('afterInit', () => {
+      if (swiper.params?.autoplay?.enabled) {
+        swiper.autoplay.stop();
+      }
+    });
+    lightbox.on('closingAnimationStart', () => {
+      const {
+        pswp
+      } = lightbox;
+      swiper.slideTo(pswp.currIndex, 0, false);
+      /* if autoplay enabled == true -> autoplay.start() when close lightbox */
+      if (swiper.params?.autoplay?.enabled) {
+        swiper.autoplay.start();
+      }
+    });
+  }
 };
 (() => {
   setupSwiper();
